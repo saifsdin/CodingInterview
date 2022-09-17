d37934dbfbd92d3c49fffe64c52b556253854a66
Md Shaif Rizvi <mrizv@cae.com>
2022-09-17 09:32:42 +0530

    update

--- DSASheet/Recursion/PascalTriangle.cpp
+++ DSASheet/Recursion/PascalTriangle.cpp
@@ -1,3 +1,26 @@
+
+/*
+https://practice.geeksforgeeks.org/problems/pascal-triangle0652
+Given a positive integer N, return the Nth row of pascal's triangle.
+Pascal's triangle is a triangular array of the binomial coefficients formed by summing up the elements of previous row.
+
+Example :
+1
+1 1
+1 2 1
+1 3 3 1
+For N = 3, return 3rd row i.e 1 2 1
+
+Example 1:
+
+Input:
+N = 4
+Output: 1 3 3 1
+Explanation: 4th row of pascal's triangle
+is 1 3 3 1.
+
+*/
+
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 
